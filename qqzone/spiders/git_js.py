906a48f6b679b5d871ed70762ca513cdb7fd7db1
Choosen001 <315333109@qq.com>
2018-09-20 19:16:13 +0800

    add git_js

---
+++ qqzone/spiders/git_js.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import scrapy
+
+class ToScrapeCSSSpider(scrapy.Spider):
+	name = "toscrape-css"
+	start_urls = ['http://www.runoob.com/git/git-tutorial.html',]
+
+	def parse(self, response):
+		with open('/home/vagrant/py.txt', 'w') as f:
+			for i in range(1, 12):
+				text = response.xpath(''//*[@id="leftcolumn"]/a[%d]/text()'%i).extract()[0].strip('\n').strip('\t')
+				f.write(text + '\n')
