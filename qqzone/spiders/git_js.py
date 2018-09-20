affdd2d91b65af7aafb813f69049e50e3905e57a
Choosen001 <315333109@qq.com>
2018-09-20 19:21:23 +0800

    test

--- qqzone/spiders/git_js.py
+++ qqzone/spiders/git_js.py
@@ -8,5 +8,5 @@ class ToScrapeCSSSpider(scrapy.Spider):
 	def parse(self, response):
 		with open('/home/vagrant/py.txt', 'w') as f:
 			for i in range(1, 12):
-				text = response.xpath(''//*[@id="leftcolumn"]/a[%d]/text()'%i).extract()[0].strip('\n').strip('\t')
+				text = response.xpath('//*[@id="leftcolumn"]/a[%d]/text()'%i).extract()[0].strip('\n').strip('\t')
 				f.write(text + '\n')
